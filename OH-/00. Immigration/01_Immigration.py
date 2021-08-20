54a6328cfff0d94ffa5e1f6a02fe63b471a9fb02
Austin Oh <oht0716@gmail.com>
2021-08-20 23:00:52 +0900

    first upload

---
+++ OH-/00. Immigration/01_Immigration.py
@@ -0,0 +1,15 @@
+# 00_Immigration_.py
+
+def solution(n, times):
+    answer = 0
+    person = n
+    while(True):
+        answer = answer + 1
+        for a in times :
+            if(answer%a == 0):
+                person = person-1
+        if(person == 0):
+            return answer
+
+if __name__ == "__main__":
+    print(solution(6, [7,10]))

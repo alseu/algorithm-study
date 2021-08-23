fcc13211a763ce8aeec94b66618632db7a239658
austin <oht0716@gmail.com>
2021-08-23 13:47:46 +0900

    일단 어찌어찌 풀긴 했는데...

--- 1st/OH-/01_Immigration.py
+++ 1st/OH-/01_Immigration.py
@@ -3,22 +3,24 @@
 
 def solution(n, times):
     answer = 0
-    smaller_value = min(times) * n
+    smaller_value = 0
     bigger_value = max(times) * n
-    while smaller_value <= bigger_value:
-        median_value = (smaller_value+bigger_value)//2
+    while( smaller_value <= bigger_value ):
+        med_value = (smaller_value+bigger_value)//2
         comp = 0
 
         for t in times:
-            comp = 
+            comp += med_value//t
+            if( comp >= n ):
+                answer = med_value
+                bigger_value = med_value - 1
+                break
 
-        if():
-            smaller_value = median_value + 1
-        else if(comp):
-            bigger_value = median_value - 1
-
+        if(comp < n):
+            smaller_value = med_value + 1
 
     return answer
+
 
 if __name__ == "__main__":
     print(solution(6, [7,10]))

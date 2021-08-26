0716a8f2c9489f5e1ae8aa307ae8dd1e0ec0c3b6
Youngwoo Lee <mvl100d@gmail.com>
2021-08-26 18:19:12 +0900

    이진탐색으로 변경해보았으나 실패-^-

--- 1st/ywlee/Immigration.py
+++ 1st/ywlee/Immigration.py
@@ -10,23 +10,38 @@
 '''
 
 def solution(n, times):
-    answer = [] 
+    answer = 0
 
     # 임의의 시간 // 각 심사관의 심사 시간 == 각 심사관별 할당 인원
-    # 심사관들의 심사 시간을 임의의 시간으로 나눈 몫을 다 더하면, n이 되어야 한다.
-    # 그런 임의의 시간들 중에 최소 시간을 탐색
+    # 임의의 시간을 심사관들의 심사 시간으로 나눈 몫을 다 더하면, n이 되어야 한다.
+    # 그런 임의의 시간들 중에 최소 시간을 탐색 -> 이진탐색
     
-    max_time = sorted(times)[len(times)-1] * n
+    # ( 1 < time < 1000000000 )
+    min_time = 1            
+    max_time = max(times) * n
 
-    for i in range(max_time) :
-        cnt = 0
+    times = sorted(times)   # 이진 탐색을 위해 정렬
+    
+    while min_time <= max_time :
+        mid = ( min_time + max_time ) // 2      # 중간값
+        cnt = 0                                 # 심사완료 인원
+        
+        # 중간값을 각 심사 시간으로 나눈 몫을 다 더한다
         for time in times :
-            cnt = cnt + ( (i+1) // time )
+            cnt = cnt + (mid // time)
 
-        if (cnt == n) :
-            answer.append(i+1)
-    
-    return sorted(answer)[0]
+        # 만약 더한 값이 n보다 작으면 (찾는 값이 오른쪽에 있음)
+        if cnt < n :
+            min_time = mid + 1
+        # 만약 더한 값이 n보다 크면 (찾는 값이 왼쪽에 있음)
+        elif cnt > n :
+            max_time = mid - 1
+        # 만약 더한 값이 n과 같으면 탈출
+        else :
+            answer = mid
+            break
+
+    return answer
 
 times = [7, 10]
 
@@ -34,15 +49,15 @@ print(solution(6, times))
 
 
 '''
-테스트 결과
+테스트 결과 
 
-테스트 1 〉	통과 (0.96ms, 10.1MB)
-테스트 2 〉	통과 (6875.40ms, 10.1MB)
-테스트 3 〉	실패 (시간 초과)
-테스트 4 〉	실패 (시간 초과)
-테스트 5 〉	실패 (시간 초과)
-테스트 6 〉	실패 (시간 초과)
-테스트 7 〉	실패 (시간 초과)
-테스트 8 〉	실패 (시간 초과)
-테스트 9 〉	실패 (시간 초과)
+테스트 1 〉	실패 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.09ms, 10.3MB)
+테스트 3 〉	실패 (2.38ms, 10.3MB)
+테스트 4 〉	실패 (291.57ms, 14.9MB)
+테스트 5 〉	실패 (390.29ms, 14.9MB)
+테스트 6 〉	실패 (430.99ms, 15.2MB)
+테스트 7 〉	실패 (461.90ms, 14.8MB)
+테스트 8 〉	실패 (537.42ms, 15MB)
+테스트 9 〉	실패 (0.03ms, 10.1MB)
 '''

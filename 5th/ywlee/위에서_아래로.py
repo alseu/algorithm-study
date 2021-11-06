07f4549675a188964c9bbd8f32ca235f1b2e090f
lyw1217 <mvl100d@gmail.com>
2021-11-06 20:57:49 +0900

    깊은 복사로 변경

--- 5th/ywlee/위에서_아래로.py
+++ 5th/ywlee/위에서_아래로.py
@@ -1,13 +1,15 @@
 # 수열이 주어지면 큰 수부터 작은 수의 순서로 정렬(내림차순)
 # 각 수를 공백으로 구분하여 출력
 # 1 <= n <= 500
+import copy
+
 n = int(input())
 arr = []
 for _ in range(n):
     arr.append(int(input()))
 
 # 선택 정렬
-cpy_arr = arr
+cpy_arr = copy.deepcopy(arr)
 for i in range(n) :
     min_idx = i
     for j in range(i+1, n) :
@@ -25,7 +27,7 @@ def printResult(arr, alg) :
 printResult(cpy_arr, '선택정렬')
 
 # 삽입 정렬
-cpy_arr = arr
+cpy_arr = copy.deepcopy(arr)
 for i in range(1, len(cpy_arr)) :
     for j in range(i, 0, -1) :
         if cpy_arr[j] < cpy_arr[j-1] :
@@ -35,6 +37,6 @@ for i in range(1, len(cpy_arr)) :
 printResult(cpy_arr, '삽입정렬')
 
 # 기본 라이브러리 정렬
-cpy_arr = arr
+cpy_arr = copy.deepcopy(arr)
 cpy_arr = sorted(arr)
 printResult(cpy_arr, '기본라이브러리')

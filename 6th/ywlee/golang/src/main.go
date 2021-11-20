96c768feac5255e6d7065183baddda97d25cebcf
lyw1217 <mvl100d@gmail.com>
2021-11-20 22:20:17 +0900

    부품찾기 golang 작성 완료

---
+++ 6th/ywlee/golang/src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"fmt"
+	//"strconv"
+	//"./bsearch"
+	"./부품찾기"
+)
+
+func main() {
+	/*
+		array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		target := 3
+		result := bsearch.BinarySearch( array, target, 0, array[len(array) - 1])
+		fmt.Println("target index = " + strconv.Itoa(result))
+
+		result = bsearch.BinarySearch2( array, target, 0, array[len(array) - 1])
+		fmt.Println("target index = " + strconv.Itoa(result))
+	*/
+	부품찾기.FindParts2()
+}

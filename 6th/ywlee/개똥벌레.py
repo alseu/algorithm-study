157a31f66fdd06d9132e484c0845cfed7ce7920a
lyw1217 <mvl100d@gmail.com>
2021-11-27 23:26:14 +0900

    개똥벌레까지

--- 6th/ywlee/개똥벌레.py
+++ 6th/ywlee/개똥벌레.py
@@ -1,4 +1,5 @@
 '''
+백준 3020번 - 개똥벌레
 N : 동굴의 길이(짝수) (2 <= N <= 200,000)
 H : 동굴의 높이 (2 <= H <= 500,000)
 첫 번째 장애물은 항상 석순, 그 다음은 종유석과 석순이 번갈아가며 나타남
@@ -8,22 +9,41 @@ from sys import stdin
 
 n, h = map(int, input().split(' '))
 
-path = [0 for _ in range(h)]
+s = []  # 석순
+j = []  # 종유석
 
 for i in range(n) :
     obs = int(stdin.readline())
     if i % 2 == 0 :
-        for j in range(obs) :
-            path[j] += 1
+        j.append(obs)
     else :
-        for j in range(h-1, h-obs-1, -1) :
-            path[j] += 1
+        s.append(obs)
 
-path.sort()
-num = 0
-min = path[0]
-for i, v in enumerate(path) :
-    if min != v :
-        num = i
-        break
-print(min, num)  
+j.sort()
+s.sort()
+
+min = n
+count = 0
+
+def binary_search( arr, target, start, end ) :
+    while start <= end :
+        # target 을 찾는다고 바로 리턴하지 않고 start <= end가 되는 지점을 찾는다
+        mid = (start + end)// 2
+        if arr[mid] < target : 
+            start = mid + 1
+        else :
+            end = mid - 1
+    
+    return start    # start 반환
+
+for i in range(1, h+1) :    # 높이(구간)가 1부터 시작하므로
+    j_count = len(j) - binary_search( j, i   - 0.5, 0, len(j)-1 )
+    s_count = len(s) - binary_search( s, h-i + 0.5, 0, len(s)-1 )
+
+    if min == j_count + s_count :
+        count += 1
+    elif min > j_count + s_count :
+        count = 1
+        min = j_count + s_count
+
+print(min, count)
